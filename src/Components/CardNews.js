03fc4fbe1a46ff0fe696a8363ef3a6d0608a3e79
brenda <bbritob8@gmail.com>
2023-09-22 10:21:44 -0300

    ajustes

--- src/Components/CardNews.js
+++ src/Components/CardNews.js
@@ -1,10 +1,10 @@
-class CardNews extends HTMLElement {
+class Cardnews extends HTMLElement {
     constructor() {
         super();
 
         const shadow = this.attachShadow({mode: "open"});
         shadow.appendChild(this.build());
-        shadow.appendChild(this.style());
+        shadow.appendChild(this.styles());
     }
 
     build(){
@@ -42,46 +42,45 @@ class CardNews extends HTMLElement {
         return componentRoot;
     }
 
-    styles(){
+    styles() {
         const style = document.createElement("style");
         style.textContent = `
             .card {
-                margin: auto;
-                width: 720px;
-                box-shadow: 9px 9px 27px 0px rgba(0,0,0,0.75);
-                -webkit-box-shadow: 9px 9px 27px 0px rgba(0,0,0,0.75);
-                -moz-box-shadow: 9px 9px 27px 0px rgba(0,0,0,0.75);
+                width: 80%;
+                box-shadow: 9px 9px 27px 0px rgba(0, 0, 0, 0.75);
+                -webkit-box-shadow: 9px 9px 27px 0px rgba(0, 0, 0, 0.75);
+                -moz-box-shadow: 9px 9px 27px 0px rgba(0, 0, 0, 0.75);
                 display: flex;
                 flex-direction: row;
                 justify-content: space-between;
             }
             
-            .card_left {
+            .card__left {
                 display: flex;
                 flex-direction: column;
                 justify-content: center;
                 padding-left: 10px;
             }
             
-            .card_left > span {
+            .card__left > span {
                 font-weight: 400;
             }
             
-            .card_left > a {
+            .card__left > a {
                 margin-top: 15px;
-                font-size: 20px;
+                font-size: 25px;
                 color: black;
                 text-decoration: none;
                 font-weight: bold;
             }
             
-            .card_left > p {
-                color: darkgray;
+            .card__left > p {
+                color: rgb(70, 70, 70);
             }
         `;
-
+    
         return style;
     }
 }
 
-customElements.define("card-news", CardNews)
+customElements.define("card-news", Cardnews)
